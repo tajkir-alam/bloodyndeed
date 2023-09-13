d30d9b587f4496c08766d987c3fffb850054ccfd
tajkir-alam <tajkir.alam@gmail.com>
2023-09-13 21:29:09 +0600

    proejct restart

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,13 @@
+"use client";
+import { Button } from "@material-tailwind/react";
 
 export default function Home() {
   return (
-    <>
-    
-    </>
+    <div className="flex w-max gap-4">
+      <Button color="blue">color blue</Button>
+      <Button color="red">color red</Button>
+      <Button color="green">color green</Button>
+      <Button color="amber">color amber</Button>
+    </div>
   )
 }
