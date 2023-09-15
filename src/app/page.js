41d6c6b94da94e614a50a7df58739cb765768a56
tajkir-alam <tajkir.alam@gmail.com>
2023-09-15 23:04:21 +0600

    nav content hidden on small device updated

--- src/app/page.js
+++ src/app/page.js
@@ -7,14 +7,14 @@ export default function Home() {
     <main>
       {/* Banner Section */}
       <section className="bg-[#ced5db]/25">
-        <div className="container flex justify-between items-center">
+        <div className="container py-14 lg:py-32 flex justify-between items-center">
           <div className="text-center w-full">
-            <Typography variant="h1" color="green" className="text-6xl">
+            <Typography variant="h1" color="green" className="text-2xl lg:text-6xl">
               Do You Know? <br /> 
               Your Blood Can <br />
               Save A Life
             </Typography>
-            <Button variant="outlined" className="mt-5 tracking-widest">Check who needs your blood</Button>
+            <Button variant="outlined" className="mt-5 tracking-widest hover:bg-black hover:text-white">Check who needs your blood</Button>
           </div>
           <div>
             <Image
@@ -22,7 +22,7 @@ export default function Home() {
               alt="donate blood"
               height={750}
               width={1200}
-              className="py-32"
+              className=" hidden lg:block"
             />
           </div>
         </div>
