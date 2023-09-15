f22789da5108f2dbc0769343044d44c35285d2c3
tajkir-alam <tajkir.alam@gmail.com>
2023-09-15 21:45:31 +0600

    banner demo added

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,31 @@
 "use client";
-import { Button } from "@material-tailwind/react";
+import { Typography } from "@material-tailwind/react";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="container">
-      <div className="flex w-max gap-4">
-        <Button color="blue">color blue</Button>
-        <Button color="red">color red</Button>
-        <Button color="green">color green</Button>
-        <Button color="amber">color amber</Button>
-      </div>
-    </div>
+    <main>
+      {/* Banner Section */}
+      <section className="bg-[#ced5db]/25">
+        <div className="container flex justify-between items-center">
+          <div className="text-center w-full">
+            <Typography variant="h1" color="green" className="text-6xl">
+              Do You Know? <br /> 
+              Your Blood Can <br />
+              Save A Life
+            </Typography>
+          </div>
+          <div>
+            <Image
+              src='/headerBanner.png'
+              alt="donate blood"
+              height={750}
+              width={1200}
+              className="py-32"
+            />
+          </div>
+        </div>
+      </section>
+    </main>
   )
 }
