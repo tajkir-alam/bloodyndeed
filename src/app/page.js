e3e9ad537512d0ee1b73c7215372755b9e14f6b1
tajkir-alam <tajkir.alam@gmail.com>
2023-09-17 00:59:58 +0600

    bannre done

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,9 @@ export default function Home() {
   return (
     <main>
       {/* Banner Section */}
-      <section className="bg-[#ced5db]/25">
+
+      {/* 1st Demo */}
+      {/* <section className="bg-[#ced5db]/25">
         <div className="container py-14 lg:py-32 flex justify-between items-center">
           <div className="text-center w-full">
             <Typography variant="h1" color="green" className="text-2xl lg:text-6xl">
@@ -26,6 +28,39 @@ export default function Home() {
             />
           </div>
         </div>
+      </section> */}
+
+      {/* 2nd Demo */}
+      {/* <section className="relative z-0 bg-contain bg-no-repeat" style={{ backgroundImage: 'url(/headerBanner.png)' }}>
+        <div className="py-[170px] text-center">
+          <Typography variant="h1" className="text-2xl lg:text-6xl text-white/90 title-shadow">
+            Do You Know? <br />
+            Your Blood Can <br />
+            Save A Life
+          </Typography>
+          <Button variant="outlined" color="white" className="mt-5 tracking-widest hover:bg-black hover:text-white shadow-white">Check who needs your blood</Button>
+        </div>
+        <div className="absolute top-0 bg-black/80 -z-10 w-full h-full"></div>
+      </section> */}
+
+
+      <section className="relative flex flex-col justify-center items-center">
+        <Image
+          src='/headerBanner.png'
+          alt="donate blood"
+          height={750}
+          width={1200}
+          className="-z-20"
+        />
+        <div className="absolute z-20 text-center">
+          <Typography variant="h1" className="text-md lg:text-6xl text-white/90 title-shadow">
+            Do You Know? <br />
+            Your Blood Can <br />
+            Save A Life
+          </Typography>
+          <Button variant="outlined" color="white" className="mt-2 text-[8px] lg:text-xs px-3 py-2 lg:px-6 lg:py-3 lg:mt-8 tracking-widest hover:bg-black hover:text-white ">Need Blood?</Button>
+        </div>
+        <div className="absolute top-0 bg-black/80 z-10 w-full h-full"></div>
       </section>
     </main>
   )
